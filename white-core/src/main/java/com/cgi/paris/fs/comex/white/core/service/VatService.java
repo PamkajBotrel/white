6845ca8abcf4647bfaa3f3f0f4a95dd5ddb5992c
Pamkaj Botrel <pamkaj.botrel@cgi.com>
2019-12-10 11:26:57 +0100

    Test VatService written

---
+++ white-core/src/main/java/com/cgi/paris/fs/comex/white/core/service/VatService.java
@@ -0,0 +1,10 @@
+package com.cgi.paris.fs.comex.white.core.service;
+
+import java.math.BigDecimal;
+
+public class VatService {
+    public static final double VAT = 0.20;
+    public int computeFrenchVat(int amount) {
+        return 200;
+    }
+}
--- white-core/src/test/java/com/cgi/paris/fs/comex/white/core/service/VatServiceTest.java
+++ white-core/src/test/java/com/cgi/paris/fs/comex/white/core/service/VatServiceTest.java
@@ -1,5 +1,13 @@
 package com.cgi.paris.fs.comex.white.core.service;
 
-public class VatServiceTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VatServiceTest {
+@Test
+    public void computeFrenchVat() {
+    final VatService vatService = new VatService();
+    assertThat(vatService.computeFrenchVat(1_000)).isEqualTo(200);
+}
 }
